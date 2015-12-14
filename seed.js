48df0b5664f60af1a7c71449180b7a44c927abbe
Lucas C. Soriani <lucas.soriani@gmail.com>
2015-12-14 20:20:56 -0200

    Update seed.js

--- seed.js
+++ seed.js
@@ -69,17 +69,17 @@ function create_medication(post_data) {
 
 	// Medications
 	var medication1 = {
-		active_agent: '', 
-		label: '', 
-		dosage: '', 
-		bula: '', 
-		type: '', 
-		intake_mean: '', 
-		name: '', 
-		br_register: '', 
-		terapeutic_class: '', 
-		manufecturer: '', 
-		receipt_ok: ''
+		active_agent: 'Nitrofural', 
+		label: 'red', 
+		dosage: '2mg', 
+		bula: 'http://www.anvisa.gov.br/datavisa/fila_bula/frmVisualizarBula.asp?pNuTransacao=786192015&pIdAnexo=2424814', 
+		type: 'ointment', 
+		intake_mean: 'Uso tópico', 
+		name: 'Furacin', 
+		br_register: '1009301370333', 
+		terapeutic_class: 'Antiinfeccioso', 
+		manufecturer: 'Mantecorp', 
+		receipt_ok: '1'
 	}
 	
 	var medication2 = {
@@ -88,7 +88,7 @@ function create_medication(post_data) {
 		dosage: '300mg', 
 		bula: 'http://www.onofre.com.br/BACKOFFICE/Uploads/Bula/020494.pdf', 
 		type: 'pill', 
-		intake_mean: 'oral', 
+		intake_mean: 'Via oral', 
 		name: 'Neosaldina', 
 		br_register: '1063902310082', 
 		terapeutic_class: 'Analgésico', 
@@ -102,7 +102,7 @@ function create_medication(post_data) {
 		dosage: '250mg', 
 		bula: 'http://www.onofre.com.br/backoffice/uploads/bula/051047.pdf', 
 		type: 'pill', 
-		intake_mean: 'oral', 
+		intake_mean: 'Via oral', 
 		name: 'Amoxil', 
 		br_register: '1010700030399', 
 		terapeutic_class: 'Antibiótico', 
@@ -117,7 +117,7 @@ function create_medication(post_data) {
 		dosage: '100mg', 
 		bula: 'http://www.medicinanet.com.br/bula/6900/nisulid_nimesulida.htm', 
 		type: 'pill', 
-		intake_mean: 'oral', 
+		intake_mean: 'Via oral', 
 		name: 'Nisulid', 
 		br_register: '1057303010052', 
 		terapeutic_class: 'Antiinflamatório', 
@@ -131,7 +131,7 @@ function create_medication(post_data) {
 		dosage: '20mg', 
 		bula: 'http://www.medley.com.br/portal/bula/Omeprazol%20capsulas.pdf', 
 		type: 'pill', 
-		intake_mean: 'oral', 
+		intake_mean: 'Via oral', 
 		name: 'Omeprazol', 
 		br_register: '1018103370038', 
 		terapeutic_class: 'Anti-úlceroso', 
@@ -196,16 +196,16 @@ function create_medication(post_data) {
 	}
 	
 	var medication10 = {
-		active_agent: 'Loratadina', 
-		label: 'none', 
-		dosage: '20mg', 
-		bula: 'http://www.medicinanet.com.br/bula/1498/claritin.htm', 
+		active_agent: 'Diclofenaco Potassico', 
+		label: 'red', 
+		dosage: '50mg', 
+		bula: 'https://portal.novartis.com.br/UPLOAD/ImgConteudos/2599.pdf', 
 		type: 'pill', 
 		intake_mean: 'Via oral', 
-		name: 'Claritin', 
-		br_register: '1009301690021', 
-		terapeutic_class: 'Alergia', 
-		manufecturer: 'MSD', 
+		name: 'Cataflam', 
+		br_register: '1006800380376', 
+		terapeutic_class: 'Antiinflamatório', 
+		manufecturer: 'Novartis', 
 		receipt_ok: '0'
 	}
 	
@@ -224,16 +224,157 @@ function create_medication(post_data) {
 	}
 	
 	var medication12 = {
-		active_agent: 'Minoxidil', 
+		active_agent: 'Diltiazem', 
 		label: 'red', 
-		dosage: '10mg', 
-		bula: 'http://www.farmadelivery.com.br/media/upload/pdf/BULAS/PFIZER/Loniten.pdf', 
+		dosage: '90mg', 
+		bula: 'http://www.boehringer-ingelheim.com.br/content/dam/internet/opu/br_PT/documents/bibrnet_bula_cardizem_sr_paciente_capsulas.pdf', 
 		type: 'pill', 
 		intake_mean: 'Via oral', 
-		name: 'Loniten', 
-		br_register: '1238900070043', 
+		name: 'Cardizem SR ', 
+		br_register: '1036700620237', 
+		terapeutic_class: 'Cardiovascular', 
+		manufecturer: 'Boehringer Ingelheim ', 
+		receipt_ok: '0'
+	}
+	
+	var medication13 = {
+		active_agent: 'Vildagliptina', 
+		label: 'red', 
+		dosage: '50mg', 
+		bula: 'https://portal.novartis.com.br/upload/imgconteudos/1813.pdf', 
+		type: 'pill', 
+		intake_mean: 'Via oral', 
+		name: 'Galvus', 
+		br_register: '1006810500048', 
+		terapeutic_class: 'Diabetes', 
+		manufecturer: 'Novartis', 
+		receipt_ok: '0'
+	}
+	
+	var medication14 = {
+		active_agent: 'Insulina Glargina', 
+		label: 'red', 
+		dosage: '100u/ml', 
+		bula: 'www.4bio.com.br/download/pdf/121/121-lantus-solostar.pdf/', 
+		type: 'liquid', 
+		intake_mean: 'Injetável', 
+		name: 'Lantus', 
+		br_register: '1006810500048', 
+		terapeutic_class: 'Diabetes', 
+		manufecturer: 'Sanofi', 
+		receipt_ok: '1'
+	}
+	
+	var medication15 = {
+		active_agent: 'Naproxeno Sódico', 
+		label: 'red', 
+		dosage: '550mg', 
+		bula: 'http://www.bayerconsumer.com.br/html/bulas/publico_geral/FLANAX_Unificada.pdf', 
+		type: 'pill', 
+		intake_mean: 'Via oral', 
+		name: 'Flanax', 
+		br_register: '1705600470122', 
+		terapeutic_class: 'Antiinflamatório', 
+		manufecturer: 'Bayer', 
+		receipt_ok: '0'
+	}
+	
+	var medication16 = {
+		active_agent: 'Rosuvastatina Calcica', 
+		label: 'red', 
+		dosage: '10mg', 
+		bula: 'http://www.anvisa.gov.br/datavisa/fila_bula/frmVisualizarBula.asp?pNuTransacao=2110532015&pIdAnexo=2502606', 
+		type: 'pill', 
+		intake_mean: 'Via oral', 
+		name: 'Trezor', 
+		br_register: '1057304140049', 
+		terapeutic_class: 'Colesterol', 
+		manufecturer: 'Ache', 
+		receipt_ok: '0'
+	}
+
+	var medication17 = {
+		active_agent: 'Cloridrato de Ambroxol', 
+		label: 'none', 
+		dosage: '30mg', 
+		bula: 'http://www.medicinanet.com.br/bula/3512/mucosolvan.htm', 
+		type: 'liquid', 
+		intake_mean: 'Via oral', 
+		name: 'Mucosolvan', 
+		br_register: '1036700730156', 
+		terapeutic_class: 'Expectorante', 
+		manufecturer: 'Boehringer Ingelheim', 
+		receipt_ok: '0'
+	}
+	
+	var medication18 = {
+		active_agent: 'Ciprofloxacino', 
+		label: 'red', 
+		dosage: '500mg', 
+		bula: 'http://www.medley.com.br/portal/bula/cloridrato_de_ciprofloxacino_comp_rev.pdf', 
+		type: 'pill', 
+		intake_mean: 'Via oral', 
+		name: 'Ciprofloxacino', 
+		br_register: '100430739', 
+		terapeutic_class: 'Antibiótico', 
+		manufecturer: 'Sandoz', 
+		receipt_ok: '1'
+	}
+
+	var medication19 = {
+		active_agent: 'Lisado Bacteriano', 
+		label: 'red', 
+		dosage: '3mg', 
+		bula: 'http://www.anvisa.gov.br/datavisa/fila_bula/frmVisualizarBula.asp?pNuTransacao=361012014&pIdAnexo=1938660', 
+		type: 'pill', 
+		intake_mean: 'Via oral', 
+		name: 'Estimoral', 
+		br_register: '1045401680024', 
+		terapeutic_class: 'Descongestionante', 
+		manufecturer: 'Daiichi Sankyo', 
+		receipt_ok: '0'
+	}
+	
+	var medication20 = {
+		active_agent: 'Paracetamol', 
+		label: 'none', 
+		dosage: '500mg', 
+		bula: 'https://www.tylenol.com.br/system/files/tylenol_500_750_0.pdf', 
+		type: 'pill', 
+		intake_mean: 'Via oral', 
+		name: 'Tylenol', 
+		br_register: '1123633260445', 
+		terapeutic_class: 'Analgésico', 
+		manufecturer: 'Johnson & Johnson', 
+		receipt_ok: '0'
+	}
+	
+	var medication21 = {
+		active_agent: 'Hemifumarato de Bisoprolol', 
+		label: 'red', 
+		dosage: '2,5mg', 
+		bula: 'http://www.merck.com.br/country.br/pt/images/Concor_Bula_Paciente_06.11.2014_tcm512_134993.pdf?Version=', 
+		type: 'pill', 
+		intake_mean: 'Via oral', 
+		name: 'Concor', 
+		br_register: '1008901940128', 
 		terapeutic_class: 'Anti-hipertensivo', 
-		manufecturer: 'Pfizer', 
+		manufecturer: 'Merck', 
+		receipt_ok: '1'
+	}	
+
+
+	var medication21 = {
+		active_agent: 'Alopurinol', 
+		label: 'red', 
+		dosage: '100mg', 
+		bula: 'http://www.netfarma.com.br/geraBula.asp?NomeArquivoBula=P00008APH00.pdf', 
+		type: 'pill', 
+		intake_mean: 'Via oral', 
+		name: 'Zyloric', 
+		br_register: '1002500780015', 
+		terapeutic_class: 'Gota', 
+		manufecturer: 'Aspen', 
 		receipt_ok: '0'
 	}	
 	// function calls
